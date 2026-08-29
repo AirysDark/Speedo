82cfcd0b6b90e35b21378b823939b1d76fc7c56e
AirysDark <megamanx2sonic@gmail.com>
2026-08-29 21:18:14 +1000

    jh

---
+++ Lib/TFT_eSPI/User_Setup.h
@@ -0,0 +1,30 @@
+#define USER_SETUP_INFO "ESP32 3.5 LCD Restore"
+
+#define ILI9488_DRIVER
+
+#define TFT_WIDTH 320
+#define TFT_HEIGHT 480
+
+#define TFT_MISO 12
+#define TFT_MOSI 13
+#define TFT_SCLK 14
+#define TFT_CS   15
+#define TFT_DC   2
+#define TFT_RST  4
+
+#define TFT_BL   27
+
+#define LOAD_GLCD
+#define LOAD_FONT2
+#define LOAD_FONT4
+#define LOAD_FONT6
+#define LOAD_FONT7
+#define LOAD_FONT8
+#define LOAD_GFXFF
+#define SMOOTH_FONT
+
+#define SUPPORT_TRANSACTIONS
+
+#define SPI_FREQUENCY       40000000
+#define SPI_READ_FREQUENCY  20000000
+#define SPI_TOUCH_FREQUENCY 2500000
